fca054e4a61c28d974b8a019a9fb16f14893fb8c
Konstantin kazaryan <kocva2003@mail.ru>
2022-10-09 21:29:34 +0800

    ADD more info to output

--- lab_dop/transport.cs
+++ lab_dop/transport.cs
@@ -20,7 +20,10 @@ namespace lab_dop
         public int radius = 0; // радиус колес
         public override String GetInfo()
         {
-            return "Я велосипед";
+            var str = "Я велосипед";
+            str += String.Format("\nтип: {0}", this.type);
+            str += String.Format("\nРадиус колес: {0}", this.radius);
+            return str;
         }
     }
     public enum CarType { bus, truck, offroad, passenger };
@@ -31,17 +34,25 @@ namespace lab_dop
         public int doors = 0; // кол-во дверей
         public override String GetInfo()
         {
-            return "Я автомобиль";
+            var str = "Я автомобиль";
+            str += String.Format("\nтип: {0}", this.type);
+            str += String.Format("\nОбъём двигателя: {0}", this.volume);
+            str += String.Format("\nкол-во дверей: {0}", this.doors);
+            return str;
         }
     }
 
+    public enum EngineType {reactive, piston}
     public class Plane : Transport
     {
-        public string engineType = ""; // тип двигателя
+        public EngineType engineType = EngineType.reactive; // тип двигателя
         public int height = 0; // максимальная высота полета
         public override String GetInfo()
         {
-            return "Я самолет";
+            var str = "Я самолет";
+            str += String.Format("\nтип двигателя: {0}", this.engineType);
+            str += String.Format("\nмакс. высота: {0}", this.height);
+            return str;
         }
     }
 }
