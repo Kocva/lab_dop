7f2dae01d44f50ebbd0aaccfe0ef5ea2ab0dbb5c
Konstantin kazaryan <kocva2003@mail.ru>
2022-10-09 20:20:49 +0800

    Добавьте файлы проекта.

---
+++ lab_dop/transport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_dop
+{
+    public enum BikeType { mountain, city, child };
+    public class Bike
+    {
+        public BikeType type = BikeType.mountain; // тип
+        public int radius = 0; // радиус колес
+    }
+    public enum CarType { bus, truck, offroad, passenger };
+    public class Car
+    {
+        public CarType type = CarType.passenger; // тип
+        public int volume = 0; // объём двигателя
+        public int doors = 0; // кол-во дверей
+    }
+
+    public class Plane
+    {
+        public string engineType = ""; // тип двигателя
+        public int height = 0; // максимальная высота полета
+    }
+}
